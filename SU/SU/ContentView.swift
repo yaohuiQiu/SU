3f0bebeef5f06979edd67b6822fb284cfa1bd187
wy_qyh <49462494+yaohuiQiu@users.noreply.github.com>
2022-09-17 15:48:11 +0800

    Add files via upload

---
+++ SU/SU/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SU
+//
+//  Created by 邱耀辉 on 2022/9/11.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SU/SU/SUApp.swift
@@ -0,0 +1,17 @@
+//
+//  SUApp.swift
+//  SU
+//
+//  Created by 邱耀辉 on 2022/9/11.
+//
+
+import SwiftUI
+
+@main
+struct SUApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
